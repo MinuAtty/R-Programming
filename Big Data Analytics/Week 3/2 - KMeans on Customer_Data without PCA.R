7112701ebafc7f35c20e5bb3c214c8c815f2577b
Minusha Attygala <136973913+MinuAtty@users.noreply.github.com>
2025-02-19 11:40:56 +0530

    Update KMeans on Customer_Data without PCA.R

--- Big Data Analytics/Week 3/2 - KMeans on Customer_Data without PCA.R
+++ Big Data Analytics/Week 3/2 - KMeans on Customer_Data without PCA.R
@@ -25,8 +25,8 @@ data$Cluster <- as.factor(results$cluster)
 ggplot(data, aes(x = Annual_Income, y = Age, color = Cluster)) +
   stat_ellipse(aes(fill = Cluster), geom = "polygon", alpha = 0.4) +  # Draw cluster boundaries with transparency
   geom_point(size = 3, alpha = 0.7) +  # Plot data points with slight transparency
-  labs(title = "Age vs Annual_Income distribution of clusters",  # Title for the plot
-       x = "Annual_Income",  # X-axis label
+  labs(title = "Age vs Annual Income distribution of clusters",  # Title for the plot
+       x = "Annual Income",  # X-axis label
        y = "Age") +  # Y-axis label
   theme_minimal()  # Use a clean and minimalistic theme for better readability
 
@@ -34,8 +34,8 @@ ggplot(data, aes(x = Annual_Income, y = Age, color = Cluster)) +
 ggplot(data, aes(x = Spending_Score, y = Age, color = Cluster)) +
   stat_ellipse(aes(fill = Cluster), geom = "polygon", alpha = 0.4) +  # Draw cluster boundaries with transparency
   geom_point(size = 3, alpha = 0.7) +  # Plot data points with slight transparency
-  labs(title = "Age vs Spending_Score distribution of clusters",  # Title for the plot
-       x = "Spending_Score",  # X-axis label
+  labs(title = "Age vs Spending Score distribution of clusters",  # Title for the plot
+       x = "Spending Score",  # X-axis label
        y = "Age") +  # Y-axis label
   theme_minimal()  # Use a minimal theme
 
@@ -43,7 +43,7 @@ ggplot(data, aes(x = Spending_Score, y = Age, color = Cluster)) +
 ggplot(data, aes(x = Annual_Income, y = Spending_Score, color = Cluster)) +
   stat_ellipse(aes(fill = Cluster), geom = "polygon", alpha = 0.4) +  # Draw cluster boundaries with transparency
   geom_point(size = 3, alpha = 0.7) +  # Plot data points with slight transparency
-  labs(title = "Annual_Income vs Spending_Score distribution of clusters",  # Title for the plot
-       x = "Annual_Income",  # X-axis label
-       y = "Spending_Score") +  # Y-axis label
+  labs(title = "Annual Income vs Spending Score distribution of clusters",  # Title for the plot
+       x = "Annual Income",  # X-axis label
+       y = "Spending Score") +  # Y-axis label
   theme_minimal()  # Use a minimal theme for better aesthetics
